26c2d0506365135bb7dc7bf216a8ee5f601b2e20
ojacquemart <o.jacquemart@gmail.com>
2013-12-21 18:57:49 +0100

    feat: new qix directive.

---
+++ src/js/directives/qix/qix.js
@@ -0,0 +1,9 @@
+angular.module('angular.library.directives.qix', [])
+   .directive('qix', function() {
+      return {
+        qix: function() {
+           return 'qix';
+        }
+      };
+   })
+;

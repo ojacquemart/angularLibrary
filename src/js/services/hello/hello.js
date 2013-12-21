c9405be4c502baed0ce64ca57eacbac324535cd3
ojacquemart <o.jacquemart@gmail.com>
2013-12-21 23:55:43 +0100

    feat(directive): remove qix directive

--- src/js/services/hello/hello.js
+++
@@ -1,9 +0,0 @@
-angular.module('angular.library.services.hello', [])
-   .service('HelloService', function() {
-      return {
-         hello: function() {
-            return 'hello world!';
-         }
-      };
-   })
-;

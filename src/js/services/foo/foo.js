95a118d2dd134a2331bc03dd685470eda55930ec
ojacquemart <o.jacquemart@gmail.com>
2013-12-22 10:36:08 +0100

    feat(services): add foo, hello and weather

---
+++ src/js/services/foo/foo.js
@@ -0,0 +1,9 @@
+angular.module('angular.library.servics.foo', [])
+   .service('FooService', function() {
+      return {
+         foo: function() {
+            return 'foo';
+         }
+      };
+   })
+;
---
+++ src/js/services/weather/weather.js
@@ -0,0 +1,9 @@
+angular.module('angular.library.services.weather', [])
+   .service('WeatherService', function() {
+      return {
+         weather: function() {
+            return 'so hot!';
+         }
+      };
+   })
+;
